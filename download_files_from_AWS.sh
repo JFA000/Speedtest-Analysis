95342e36090f831da6273a5cac8698dd90e3c5a1
JFA000 <jonatasfernandesandrade@gmail.com>
2024-06-17 01:48:50 -0300

    Adicionando primeiro trimestre de 2024 ao arquivo de download

--- download_files_from_AWS.sh
+++ download_files_from_AWS.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Instalar AWS CLI
-pip install awscli
+# Instalar AWS CLI - caso necessário
+#pip install awscli
 
-# Configurar AWS CLI
-aws configure
+# Configurar AWS CLI - caso necessário
+#aws configure
 
 # Baixar arquivos da Amazon S3
 aws s3 cp --no-sign-request s3://ookla-open-data/parquet/performance/type=fixed/year=2023/quarter=1/2023-01-01_performance_fixed_tiles.parquet ./
 aws s3 cp --no-sign-request s3://ookla-open-data/parquet/performance/type=fixed/year=2023/quarter=2/2023-04-01_performance_fixed_tiles.parquet ./
 aws s3 cp --no-sign-request s3://ookla-open-data/parquet/performance/type=fixed/year=2023/quarter=3/2023-07-01_performance_fixed_tiles.parquet ./
 aws s3 cp --no-sign-request s3://ookla-open-data/parquet/performance/type=fixed/year=2023/quarter=4/2023-10-01_performance_fixed_tiles.parquet ./
+aws s3 cp --no-sign-request s3://ookla-open-data/parquet/performance/type=fixed/year=2024/quarter=1/2024-01-01_performance_fixed_tiles.parquet ./
